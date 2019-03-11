ce9db7427a3efd16e40186738b95e4344f456be0
Sandeep Kumar Gupta <sandeepkumar.g@imimobile.com>
2019-03-11 15:30:36 +0530

    ng clickoutside bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+import {catchError, map} from 'rxjs/operators';
+import {Observable} from 'rxjs';
 
+export interface News {
+  heading:string,
+  text:string
+}
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+      <div (clickOutside)="onClickedOutside($event)">
+          clickOutside will not work outside of red border
+      </div>
+  `,
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-test';
+export class AppComponent implements OnInit{
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  onClickedOutside(){
+    alert("hello");
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {ClickOutsideModule} from 'ng-click-outside';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ClickOutsideModule
   ],
   providers: [],
   bootstrap: [AppComponent]
